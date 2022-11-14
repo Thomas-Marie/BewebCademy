b86b87fab57f4e3d6078a78f0389554128451e6e
Elypsenbwb <bwb-y.bacon@fondespierre.com>
2022-11-14 12:24:44 +0100

    bases file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home"
+import Dashboard from "./pages/user/dashboard"
+import Exercice from "./pages/user/exercice"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/user" element={<Dashboard />} />
+      <Route path="/user/exercice" element={<Exercice />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/user/exercice.tsx
@@ -0,0 +1,5 @@
+export default function exercice(){
+    return(
+        <div>cc</div>
+    )
+}
