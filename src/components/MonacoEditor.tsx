b9965cd211b1a846d52231539799b681201f334d
Elypsenbwb <bwb-y.bacon@fondespierre.com>
2022-11-15 14:04:39 +0100

    ajustement sur le visuel

--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -1,146 +1,169 @@
-import Editor from "@monaco-editor/react";
-import { Button, ButtonGroup, Divider } from "@mui/material";
-import { Box, Container } from "@mui/system";
+import Editor, { Monaco } from "@monaco-editor/react";
+import { Divider, Button, IconButton } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { useRef, useState } from "react";
-import ReactDOM from "react-dom";
+
 import Language from "../services/LanguageService";
 import { styled } from "@mui/material/styles";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import CachedIcon from "@mui/icons-material/Cached";
-import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import OpenInFullIcon from "@mui/icons-material/OpenInFull";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
-const EditorBtn = styled(Button)({
-  backgroundColor: "#1d1d1b",
-  color: "#ffffff",
-  borderRadius: 0,
-  border: "0 solid #1d1d1b",
-  "&:disabled": {
-    color: "#db1144",
-  },
-  "&:hover": {
-    borderColor: "#db1144",
-  },
+//////////////Styled components//////////////////////////////////////
+const EditorButton = styled(Button)({
+    backgroundColor: "#1d1d1b",
+    color: "#ffffff",
+    borderRadius: 0,
+    height: "100%",
+
+    border: "0 solid #1d1d1b",
+    "&:disabled": {
+        color: "#db1144",
+    },
+    "&:hover": {
+        borderColor: "#db1144",
+    },
 }) as typeof Button;
 
+const EditorIcon = styled(IconButton)({
+    height: "100%",
+}) as typeof IconButton;
+//////////////////////////////////////////////////////////////////
+
+/////////////////FakeData/////////////////////////
 const files: any = {
-  "script.js": {
-    name: "script.js",
-    language: "javascript",
-    value: "function caca(){return 'prout'} ",
-  },
-  "style.css": {
-    name: "style.css",
-    language: "css",
-    value: "",
-  },
-  "index.html": {
-    name: "index.html",
-    language: "html",
-    value: "<h1>Coucou</hi>",
-  },
+    "script.js": {
+        name: "script.js",
+        language: "javascript",
+        value: "function caca(){return 'prout'} ",
+    },
+
+    "index.html": {
+        name: "index.html",
+        language: "html",
+        value: "<h1>Coucou</hi>",
+    },
 };
 
 export default function MonacoEditor(/*language: Language*/) {
-  const editorRef: any = useRef(null);
+    const editorRef: any = useRef(null);
 
-  function handleEditorDidMount(editor: any, monaco: any): void {
-    editorRef.current = editor;
-  }
+    function handleEditorDidMount(editor: any, monaco: Monaco): void {
+        editorRef.current = editor;
+    }
 
-  const [fileName, setFileName] = useState("script.js");
+    const [fileName, setFileName] = useState<string>("script.js");
 
-  function showValue() {
-    return editorRef.current.getValue();
-  }
+    function showValue() {
+        return editorRef.current.getValue();
+    }
 
-  const file: any = files[fileName];
+    const file: any = files[fileName];
 
-  return (
-    <Box
-      sx={{
-        backgroundColor: "#db1144",
-        color: "#ffffff",
-        borderRadius: 0,
+    function expandEditor() {
+        console.log(" a venir bientot peut etre")
+    }
 
-        mt: 10,
-        width: "33vw",
-        height: "80vh",
-        ml: 20,
-      }}
-    >
-      <Box display="flex" justifyContent={"space-between"} flexDirection="row">
-        <Box>
-          <EditorBtn
-            sx={{ backgroundColor: "#1d1d1b" }}
-            disabled={fileName === "script.js"}
-            onClick={() => setFileName("script.js")}
-          >
-            script.js
-          </EditorBtn>
-          <EditorBtn
-            disabled={fileName === "style.css"}
-            onClick={() => setFileName("style.css")}
-          >
-            style.css
-          </EditorBtn>
-          <EditorBtn
-            disabled={fileName === "index.html"}
-            onClick={() => setFileName("index.html")}
-          >
-            index.html
-          </EditorBtn>
-        </Box>
-        <Box display="flex">
-            <Divider flexItem={true} orientation="vertical" light={true} variant="fullWidth" sx={{ backgroundColor: "white"}}/>
-
-          <Button  sx={{ color: "white", border: "2 solid white", maxWidth: 2 }}>
-            <OpenInFullIcon sx={{ border: "2 solid white" , }} />
-          </Button>
-        </Box>
-      </Box>
-      <Editor
-        height="70vh"
-        width="33vw"
-        onMount={handleEditorDidMount}
-        theme="vs-dark"
-        path={file.name}
-        defaultLanguage={file.language}
-        defaultValue={file.value}
-        onChange={showValue}
-      />
-      <Box display="flex" justifyContent={"space-between"}>
-        <Box>
-          <Button
-            variant="contained"
-            size="small"
+    return (
+        <Box
             sx={{
-              backgroundColor: "#1d1d1b",
-
-              maxWidth: 5,
-              maxHeight: 25,
-              "&:hover": { backgroundColor: "#ffffff", color: "black" },
-              ml: 3,
+                color: "#ffffff",
+                borderRadius: 0,
+                mt: 10,
+                width: "33vw",
+                height: "80vh",
+                ml: 20,
             }}
-          >
-            {" "}
-            Exécuter{" "}
-          </Button>
+        >
+            <Box
+                display="flex"
+                justifyContent={"space-between"}
+                flexDirection="row"
+                sx={{ backgroundColor: "#db1144" }}
+            >
+                <Box sx={{ width: "93%" }}>
+                    <EditorButton
+                        disabled={fileName === "script.js"}
+                        onClick={() => setFileName("script.js")}
+                    >
+                        script.js
+                    </EditorButton>
 
-          <Button size="small" sx={{ color: "white" }}>
-            <ContentCopyIcon />
-          </Button>
-          <Button size="small" sx={{ color: "white", m: -2 }}>
-            <CachedIcon />
-          </Button>
+                    <EditorButton
+                        disabled={fileName === "index.html"}
+                        onClick={() => setFileName("index.html")}
+                    >
+                        index.html
+                    </EditorButton>
+                </Box>
+                <Box display="flex" width={"7%"} height={"100%"}>
+                    <Divider
+                        flexItem={true}
+                        orientation="vertical"
+                        sx={{ backgroundColor: "white" }}
+                    />
+
+                    <EditorIcon onClick={expandEditor}
+                        sx={{
+                            color: "white",
+                            border: "2 solid white",
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <OpenInFullIcon  />
+                    </EditorIcon>
+                </Box>
+            </Box>
+            <Editor
+                height="70vh"
+                width="33vw"
+                onMount={handleEditorDidMount}
+                theme="vs-dark"
+                path={file.name}
+                defaultLanguage={file.language}
+                defaultValue={file.value}
+                onChange={showValue}
+            />
+            <Box
+                display="flex"
+                justifyContent={"space-between"}
+                alignItems="center"
+                sx={{ backgroundColor: "#db1144" }}
+            >
+                <Box height={"100%"}>
+                    <Button
+                        variant="contained"
+                        size="small"
+                        sx={{
+                            backgroundColor: "#1d1d1b",
+
+                            maxWidth: 5,
+                            maxHeight: 25,
+                            "&:hover": {
+                                backgroundColor: "#ffffff",
+                                color: "black",
+                            },
+                            ml: 3,
+                        }}
+                    >
+                        Exécuter
+                    </Button>
+
+                    <EditorIcon size="small" sx={{ color: "white" }}>
+                        <ContentCopyIcon />
+                    </EditorIcon>
+                    <EditorIcon size="small" sx={{ color: "white" }}>
+                        <CachedIcon />
+                    </EditorIcon>
+                </Box>
+                <Box>
+                    <EditorIcon sx={{ color: "white" }}>
+                        <VisibilityIcon />
+                    </EditorIcon>
+                </Box>
+            </Box>
         </Box>
-        <Box>
-          <Button sx={{ color: "white", right: 0 }}>
-            <VisibilityIcon />
-          </Button>
-        </Box>
-      </Box>
-    </Box>
-  );
+    );
 }
