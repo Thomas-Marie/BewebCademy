d609c64c0ce34881e7aa136ecb4c59c45d802d22
knut5 <lucas.steichen@gmail.com>
2022-11-15 16:17:42 +0100

    Structure app done

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './theme/bewebcademy.scss';
+
+
+const Button: React.FC = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Button;
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './theme/bewebcademy.scss';
+
+
+const Navbar: React.FC = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Navbar;
