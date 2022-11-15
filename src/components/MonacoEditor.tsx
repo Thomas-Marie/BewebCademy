248ba0117fb8c87b175bd0bdf61682f291262f85
Elypsenbwb <bwb-y.bacon@fondespierre.com>
2022-11-15 11:38:23 +0100

    visuel terminé

--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -1,23 +1,27 @@
 import Editor from "@monaco-editor/react";
-import { Button, ButtonGroup } from "@mui/material";
-import { Container } from "@mui/system";
+import { Button, ButtonGroup, Divider } from "@mui/material";
+import { Box, Container } from "@mui/system";
 import React, { useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import Language from "../services/LanguageService";
 import { styled } from "@mui/material/styles";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import CachedIcon from "@mui/icons-material/Cached";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import OpenInFullIcon from "@mui/icons-material/OpenInFull";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const EditorBtn = styled(Button)({
-    backgroundColor: "#1d1d1b",
-    color: "#ffffff",
-    borderRadius: 0,
-    border: "0 solid #1d1d1b",
-    "&:disabled":{
-        color: "#db1144"
-    },
-    "&:hover":{
-        borderColor:"#db1144",
-        
-    },
+  backgroundColor: "#1d1d1b",
+  color: "#ffffff",
+  borderRadius: 0,
+  border: "0 solid #1d1d1b",
+  "&:disabled": {
+    color: "#db1144",
+  },
+  "&:hover": {
+    borderColor: "#db1144",
+  },
 }) as typeof Button;
 
 const files: any = {
@@ -39,52 +43,63 @@ const files: any = {
 };
 
 export default function MonacoEditor(/*language: Language*/) {
-    const editorRef:any = useRef(null);
+  const editorRef: any = useRef(null);
 
-    function handleEditorDidMount(editor: any, monaco: any): void {
+  function handleEditorDidMount(editor: any, monaco: any): void {
     editorRef.current = editor;
-    }
-    
-    const [fileName, setFileName] = useState("script.js");
-    
-    function showValue(){
-        return editorRef.current.getValue();
-    }
-    
-    const file: any = files[fileName];
+  }
 
-    return (
-    <Container maxWidth="sm" sx={{
-        backgroundColor:  "#db1144",
+  const [fileName, setFileName] = useState("script.js");
+
+  function showValue() {
+    return editorRef.current.getValue();
+  }
+
+  const file: any = files[fileName];
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#db1144",
         color: "#ffffff",
         borderRadius: 0,
-        
+
         mt: 10,
-        width:"33vw",
-        height:"80vh"
-        }}>
-            
-      <ButtonGroup >
-        <EditorBtn
-          sx={{ backgroundColor: "#1d1d1b" }}
-          disabled={fileName === "script.js"}
-          onClick={() => setFileName("script.js")}
-        >
-          script.js
-        </EditorBtn>
-        <EditorBtn
-          disabled={fileName === "style.css"}
-          onClick={() => setFileName("style.css")}
-        >
-          style.css
-        </EditorBtn>
-        <EditorBtn
-          disabled={fileName === "index.html"}
-          onClick={() => setFileName("index.html")}
-        >
-          index.html
-        </EditorBtn>
-      </ButtonGroup>
+        width: "33vw",
+        height: "80vh",
+        ml: 20,
+      }}
+    >
+      <Box display="flex" justifyContent={"space-between"} flexDirection="row">
+        <Box>
+          <EditorBtn
+            sx={{ backgroundColor: "#1d1d1b" }}
+            disabled={fileName === "script.js"}
+            onClick={() => setFileName("script.js")}
+          >
+            script.js
+          </EditorBtn>
+          <EditorBtn
+            disabled={fileName === "style.css"}
+            onClick={() => setFileName("style.css")}
+          >
+            style.css
+          </EditorBtn>
+          <EditorBtn
+            disabled={fileName === "index.html"}
+            onClick={() => setFileName("index.html")}
+          >
+            index.html
+          </EditorBtn>
+        </Box>
+        <Box display="flex">
+            <Divider flexItem={true} orientation="vertical" light={true} variant="fullWidth" sx={{ backgroundColor: "white"}}/>
+
+          <Button  sx={{ color: "white", border: "2 solid white", maxWidth: 2 }}>
+            <OpenInFullIcon sx={{ border: "2 solid white" , }} />
+          </Button>
+        </Box>
+      </Box>
       <Editor
         height="70vh"
         width="33vw"
@@ -94,8 +109,38 @@ export default function MonacoEditor(/*language: Language*/) {
         defaultLanguage={file.language}
         defaultValue={file.value}
         onChange={showValue}
-        
       />
-    </Container>
+      <Box display="flex" justifyContent={"space-between"}>
+        <Box>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              backgroundColor: "#1d1d1b",
+
+              maxWidth: 5,
+              maxHeight: 25,
+              "&:hover": { backgroundColor: "#ffffff", color: "black" },
+              ml: 3,
+            }}
+          >
+            {" "}
+            Exécuter{" "}
+          </Button>
+
+          <Button size="small" sx={{ color: "white" }}>
+            <ContentCopyIcon />
+          </Button>
+          <Button size="small" sx={{ color: "white", m: -2 }}>
+            <CachedIcon />
+          </Button>
+        </Box>
+        <Box>
+          <Button sx={{ color: "white", right: 0 }}>
+            <VisibilityIcon />
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
