bb156c3581cffce4c932d2b6b8031e85a5c1495b
thomas997 <thomus97@gmail.com>
2022-11-17 20:37:10 +0100

    adjustement components Item deleted and image width

--- src/components/layout/BadgeList.tsx
+++ src/components/layout/BadgeList.tsx
@@ -1,25 +1,9 @@
 import * as React from 'react';
-import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import {useEffect, useState} from 'react';
 import {getBadges} from '../../services/badge.service'
 import { Typography } from '@mui/material';
-
-// --------------------------------Styled components --------------------------------
-// const Item = styled(Paper)(({ theme }) => ({
-
-//   ...theme.typography.body2,
-//   padding: theme.spacing(2),
-//   textAlign: 'center',
-//   justifyContent: 'center',
-//   color: theme.palette.text.secondary,
-// }));
-// @mui/material/Paper -> Paper.js PaperRoot et faire des test pour modifier / supprimer des propriétés css.
-// ----------------------------------------------------------------------------------
-
-// Possibilité de mettre plusieurs fonctions dans le même component?
 
 export default function BadgeList() {
 
@@ -32,16 +16,18 @@ const [badges, setBadges] = useState<any>([])
     }
     fetchBadges().catch(console.error)
   }, [])
+
   console.log(badges)
+
   return (
     <Box sx={{ flexGrow: 1, ml:6 }}>
-      <h2>Badges({badges.length}) </h2>
-      <Grid container columns={{ xs: 4, sm: 8, md: 8 }}>
+      <Typography variant="h3">Badges({badges.length}) </Typography>
+      <Grid container columns={{ xs: 4, sm: 6, md: 8 }}>
         {badges.map((badge: any, index: any) => (
           <Grid xs={2} sm={2} md={2} key={index}>
                 <Box>
                   <Box display="flex" justifyContent="center" alignItems="center">
-                    <img src="https://logospng.org/download/javascript/logo-javascript-icon-1024.png" width="30%"/>
+                    <img src="https://logospng.org/download/javascript/logo-javascript-icon-1024.png" width="100px"/>
                   </Box>
                   <Box display="flex" justifyContent="center" alignItems="center">
                     <Typography>{badge.name}</Typography>
