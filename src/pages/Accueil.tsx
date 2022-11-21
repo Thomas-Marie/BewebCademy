9ca4f8855b9b7453b785cee01cfb093335a5632f
Jarod Wuillaume <nova@MBPdeWuillaume.lan>
2022-11-21 16:53:13 +0100

    test

--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -1,20 +1,26 @@
-import { Box, Tabs, Tab } from "@mui/material";
+import { Box, Tabs, Tab, Grid, Button } from "@mui/material";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import React, { useEffect, useState } from "react";
 import { useKeycloak } from "@react-keycloak/web";
 import { createSession, getSessionByUserId, getSessions } from "../services/session.service";
 import User from "../models/User";
+import { checkDreaftOpen } from "../services/draft.service";
 
-const Accueil = () => {
-    
-    const { keycloak, initialized } = useKeycloak();
+const Accueil: React.FC = () => {
 
-    function infoUser(){
-      console.log(keycloak.tokenParsed);
-    }
+  //boolean to check if draft open or not
+  const [draftOpen, setDraftOpen] = useState(false);
 
-    const checkSession = async () => {
+  checkDreaftOpen().then((res) => {
+    setDraftOpen(res);
+    console.log("draftOpen", draftOpen);
+  });
+
+  const { keycloak, initialized } = useKeycloak();
+
+  const checkSession = async () => {
+    if (keycloak.authenticated) {
       let user : User = {
         id: keycloak.tokenParsed?.sub || "",
         username: keycloak.tokenParsed?.preferred_username || "",
@@ -36,38 +42,56 @@ const Accueil = () => {
         console.log(newSession);
       }
     }
-    
-    useEffect(() => {
-      if (initialized) {
-        checkSession();
-      }
-    }, [initialized]);
+    window.location.href = 'profil';
+  }
+
+  const login = () => {
+    keycloak.login();
+  }
+  
+  useEffect(() => {
+    if (initialized) {
+      checkSession();
+    }
+  }, [initialized]);
 
 
-  return (
-    <div>
-        {!keycloak.authenticated && (
-            <button
-              type="button"
-              className="text-blue-800"
-              onClick={() => keycloak.login()}
-            >
-              Login
-            </button>
-            
-          )}
-           {!!keycloak.authenticated && (
-                   <button
-                     type="button"
-                     className="text-blue-800"
-                     onClick={() => keycloak.logout()}
-                   >
-                     Logout ({keycloak.tokenParsed?.preferred_username})
-                   </button>
-                 )}
-                 <button onClick={() => infoUser()}>info</button>
-    </div>
-  )
+return (
+  <div>
+      <Box sx={{ width: '100%', typography: 'body1', height: '97vh' }}>
+        {!draftOpen 
+        ? 
+        (
+          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} height="100%">
+            <Grid item xs={6} display="flex" justifyContent="center" alignItems="center"> 
+              <img src="https://i.ibb.co/7ND1qzz/gcm-Rp-J7-400x400-removebg-preview.png" />
+            </Grid>
+            <Grid item xs={6} display="flex" justifyContent="center" alignItems="center">
+              <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid>
+                  <h1 style={{margin: "50px"}}>Une période de préparation pour une draft est en cours. Vous pouvez participer pour avoir une chance d'être sélectionné. Il vous faut juste réaliser les exercices et obtenir des badges.</h1>
+                </Grid>
+                <Grid>
+                  <Button style={{margin: "50px"}} onClick={login} variant="contained" sx={{bgcolor: '#db1144', '&:hover': {bgcolor: '#1d1d1b'}}}>  Participez a la draft. </Button> 
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        ) : 
+        (
+          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} height="100%">
+            <Grid item xs={6} display="flex" justifyContent="center" alignItems="center"> 
+              <img src="https://i.ibb.co/7ND1qzz/gcm-Rp-J7-400x400-removebg-preview.png" />
+            </Grid>
+            <Grid item xs={6} display="flex" justifyContent="center" alignItems="center">
+              <h1 style={{margin: "100px"}}>Il n'y a pas de période de draft en cours, revenez plus tard.</h1>
+            </Grid>
+          </Grid>
+        )}
+      </Box>
+    <Footer />
+  </div>
+);
 }
 
 export default Accueil;
