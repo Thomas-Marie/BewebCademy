7741a945a122e30a6bb0c13329aad100bbf3ce33
thomas997 <thomus97@gmail.com>
2022-11-21 15:12:43 +0100

    session modified

--- src/models/session.ts
+++ src/models/session.ts
@@ -1,12 +1,10 @@
+import User from "./user"
 import Badge from "./badge"
 import Exercice from "./exercice"
-import User from "./user"
 
-
-export default interface Session{
-    _id: string,
-    badges: Badge[],
-    exercices: Exercice[],
-    user: User
-
+export default interface Session {
+    "_id": string
+    "badges": Badge[],
+    "exercices": Exercice[],
+    "user": User
 }
